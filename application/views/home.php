344b0f9a82f509ca7cd07c3dbbc85c900fcce386
aj1296 <aj.alcantara6@gmail.com>
2017-09-09 00:29:05 +0800

    Eto na dieths

--- application/views/home.php
+++ application/views/home.php
@@ -30,47 +30,13 @@ defined ('BASEPATH') OR exit ('No direct script access allowed');
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<style>
-/* The Modal (background) */
-.modal {
-    display: none; /* Hidden by default */
-    position: fixed; /* Stay in place */
-    z-index: 1; /* Sit on top */
-    padding-top: 100px; /* Location of the box */
-    left: 0;
-    top: 0;
-    width: 100%; /* Full width */
-    height: 100%; /* Full height */
-    overflow: auto; /* Enable scroll if needed */
-    background-color: rgb(0,0,0); /* Fallback color */
-    background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
-}
-
-/* Modal Content */
-.modal-content {
-    background-color: #fefefe;
-    margin: auto;
-    padding: 20px;
-    border: 1px solid #888;
-    width: 80%;
-}
-
-/* The Close Button */
-.close {
-    color: #aaaaaa;
-    float: right;
-    font-size: 28px;
-    font-weight: bold;
-}
-
-.close:hover,
-.close:focus {
-    color: #000;
-    text-decoration: none;
-    cursor: pointer;
-}
-
-</style>
+	#inquiry
+	{
+		cursor:pointer;
+	}
+	</style>
 </head>
 
 <body>
@@ -115,15 +81,28 @@ defined ('BASEPATH') OR exit ('No direct script access allowed');
                             </li>
                             </ul>
                         <li>
-                            <a href=" #myModal" data-toggle="modal">Open Modal</a>                 
-                        </li>
+                             <p class="navbar-text" id= "inquiry" href="#myModal" data-toggle="modal" data-target="#myModal">Inquiry</p>
+                        </li> 
 					
-            </div>
             <!-- /.navbar-collapse -->
         </div>
         <!-- /.container -->
     </nav>
 
+<div class="modal fade" id="myModal">
+	<div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+        </div>
+        <div class="modal-body">
+        <div class="modal-footer">
+
+        </div>
+      </div>
+    </div>
+</div>
+
+</div>
     <!-- Header Carousel -->
     <header id="myCarousel" class="carousel slide">
         <!-- Indicators -->
@@ -315,32 +294,13 @@ defined ('BASEPATH') OR exit ('No direct script access allowed');
 	
 	
 	<script>
-// Get the modal
-var modal = document.getElementById('myModal');
+	$(document).ready(function(){
+    $("#myBtn").click(function(){
+        $("#myModal").modal();
+		});
+	});
+	</script>
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-</script>
 </body>
 
 </html>
